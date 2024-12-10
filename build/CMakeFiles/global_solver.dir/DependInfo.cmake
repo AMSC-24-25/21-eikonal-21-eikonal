4b2d666254086f3830e5d480d4b9f06202c95a51
NicoNovi9 <nicolanoventa9@gmail.com>
2024-12-10 12:11:21 +0100

    Add initial CMake configuration and main solver implementation

---
+++ build/CMakeFiles/global_solver.dir/DependInfo.cmake
@@ -0,0 +1,27 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/nico/21-eikonal-21-eikonal/LocalProblem/DescentDirectionFactory.cpp" "CMakeFiles/global_solver.dir/LocalProblem/DescentDirectionFactory.cpp.o" "gcc" "CMakeFiles/global_solver.dir/LocalProblem/DescentDirectionFactory.cpp.o.d"
+  "/home/nico/21-eikonal-21-eikonal/LocalProblem/DescentDirections.cpp" "CMakeFiles/global_solver.dir/LocalProblem/DescentDirections.cpp.o" "gcc" "CMakeFiles/global_solver.dir/LocalProblem/DescentDirections.cpp.o.d"
+  "/home/nico/21-eikonal-21-eikonal/LocalProblem/LineSearchSolver.cpp" "CMakeFiles/global_solver.dir/LocalProblem/LineSearchSolver.cpp.o" "gcc" "CMakeFiles/global_solver.dir/LocalProblem/LineSearchSolver.cpp.o.d"
+  "/home/nico/21-eikonal-21-eikonal/LocalProblem/solveEikonalLocalProblem.cpp" "CMakeFiles/global_solver.dir/LocalProblem/solveEikonalLocalProblem.cpp.o" "gcc" "CMakeFiles/global_solver.dir/LocalProblem/solveEikonalLocalProblem.cpp.o.d"
+  "/home/nico/21-eikonal-21-eikonal/src/main_global_solver.cpp" "CMakeFiles/global_solver.dir/src/main_global_solver.cpp.o" "gcc" "CMakeFiles/global_solver.dir/src/main_global_solver.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
